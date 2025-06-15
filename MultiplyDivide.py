680b12828bb377d033d5474bc40bd1750b6366cc
kstaudt31 <100602105+kstaudt31@users.noreply.github.com>
2025-06-15 18:37:23 -0400

    Add files via upload

---
+++ MultiplyDivide.py
@@ -0,0 +1,9 @@
+#Ask user to input two numbers
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+#Multiply two numbers together and output answer
+print("The two numbers multiplied together equals", num1 * num2)
+
+#Divide two numbers and output answer
+print("The two numbers divided together equals", num1 / num2)
