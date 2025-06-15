ea10c89e2f3af93a0cf022c4a7bf6b799c7690c8
kstaudt31 <100602105+kstaudt31@users.noreply.github.com>
2025-06-15 18:36:47 -0400

    Add files via upload

---
+++ AddSubtract.py
@@ -0,0 +1,9 @@
+#Ask user to input two numbers
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+#Add two numbers together and output answer
+print("The two numbers added together equals", num1 + num2)
+
+#Subtract two numbers and output answer
+print("The two numbers subtracted together equals", num1 - num2)
